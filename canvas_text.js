29e719931dd49af498cc8cd1a17054286f32a48f
CarmineC93 <107880239+CarmineC93@users.noreply.github.com>
2023-01-04 16:43:10 +0100

    with async function the letter change with keyUp event

--- canvas_text.js
+++ canvas_text.js
@@ -4,17 +4,20 @@ const settings = {
   dimensions: [ 1080, 1080]
 };
 
+let text  ='A';
+let fontSize = 1200;
+let fontFamily = 'serif';
+
 const sketch = () => {
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
     context.fillStyle = 'black';
-    context.font = '1200px serif';
+    context.font = `${fontSize}px ${fontFamily}`;
     context.textBaseline = 'top';
     //context.textAlign = 'center';
 
-    const text  ='A';
 
     const metrics = context.measureText(text);
     const mx = metrics.actualBoundingBoxLeft * -1;
@@ -22,19 +25,35 @@ const sketch = () => {
     const mw = metrics.actualBoundingBoxLeft +   metrics.actualBoundingBoxRight;
     const mh =  metrics.actualBoundingBoxAscent +   metrics.actualBoundingBoxDescent;
 
+    const x = (width - mw) * 0.5 - mx;
+    const y = (height - mh) * 0.5 - my;
+
 
     context.save()
-   // context.translate(width * 0.5, height * 0.5);
+    context.translate(x, y);
 
 
    context.beginPath();
    context.rect(mx, my, mw, mh);
    context.stroke();
 
-    context.fillText('A', 0, 0);
+    context.fillText(text, 0, 0);
     context.restore();
 
   };
 };
 
-canvasSketch(sketch, settings);
+
+const onKeyUp = (e) => {
+  console.log(e);
+  text = e.key;
+  manager.render();
+};
+
+document.addEventListener('keyup' , onKeyUp);
+
+const start = async () => {
+  manager = await canvasSketch ( sketch, settings);
+};
+
+start();
